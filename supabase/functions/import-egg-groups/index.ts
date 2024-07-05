1021620dca6a09e762b29f7b164f3e35d212e5b0
Phil Armstrong <phil.armstrong@itv.com>
2024-07-05 20:10:14 +0100

    remove select

--- supabase/functions/import-egg-groups/index.ts
+++ supabase/functions/import-egg-groups/index.ts
@@ -37,8 +37,7 @@ Deno.serve(async (req) => {
         const itemsToUpdate = pokeapiResponse.map(rawObject => ({ id: rawObject.id, name: rawObject.name }));
 
         const { data, error } = await supabaseClient.from('egg_group')
-        .insert(itemsToUpdate)
-        .select();
+        .insert(itemsToUpdate);
 
         console.log(data);
         console.log(error);
